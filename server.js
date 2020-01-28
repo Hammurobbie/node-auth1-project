ed7a53fcd40b9c7a7c82d041cf15a078c36f18fc
Robbie Porter <rthomporter@gmail.com>
2020-01-28 16:39:15 -0600

    MVP + a lil stretch

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const usersRouter = require("./Users/users-router");
 
 const session = require("express-session");
 
+// const dbConnection = require("./data/db-config");
+
+// const KnexSessionStore = require("connect-session-knex")(session);
+
 const server = express();
 
 server.use(express.json());
+
+server.use(cors());
 
 server.use(
   session({
@@ -14,11 +22,18 @@ server.use(
     secret: "best not spill yer beans!",
     cookie: {
       maxAge: 1 * 24 * 60 * 60 * 1000,
-      secure: true
+      secure: false
     },
-    httpOnly: true,
+    httpOnly: false,
     resave: false,
     saveUninitialized: false
+    // store: new KnexSessionStore({
+    //   knex: dbConnection,
+    //   tablename: "sessions",
+    //   sidFieldName: "sid",
+    //   createTable: true,
+    //   clearInterval: 60000,
+    // })
   })
 );
 
